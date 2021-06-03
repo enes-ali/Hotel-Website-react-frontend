d3475647d4a68940625faa8cf1c6df3526133de9
nearFuture00 <enes.ali.karakobak@gmail.com>
2021-06-03 22:43:01 +0300

    Change Navbar Style. backdrop-filter doesn't work on all browser. because of that changed colors to solid colors

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Styles/Navbar.css";
 import "../Styles/Global.css";
-import BrandImage from "../Sources/icons/Logo-Black.png";
+import BrandImage from "../Sources/icons/Logo.png";
 import PhoneIcon from "../Sources/icons/phone-solid.svg";
 import DropdownMenu from "./DropdownMenu";
 import {Link} from "react-router-dom";
@@ -9,20 +9,38 @@ import {Link} from "react-router-dom";
 
 class Navbar extends React.Component{
 
+	state = {
+		nav_class: ""
+	}
+
 	constructor()
 	{
 		super();
-		this.container_style = { height: "100%" };
+		window.addEventListener("scroll", this.onScroll);
+	}
+
+	onScroll = (event) => {
+		console.log(window.scrollY);
+		if(window.scrollY > 100){
+			this.setState({
+				nav_class: "navbar-active"
+			});
+		}
+		else{
+			this.setState({
+				nav_class: ""
+			});
+		}
 	}
 
 	render()
 	{
 		return(
-			<nav className="row space-between align-vertical">
+			<nav className={"row space-between align-vertical " + this.state.nav_class }>
 				<Link to="/"> <img src={BrandImage} alt="NAGIDOS HOTEL" id="brand-image" /> </Link>
 				
 				<div className="row align-center full-height">
-					<div id="call" className="row align-center">
+					<div id="call" className="row ">
 						<span>+90 536 865 07 65</span>
 						<img src={PhoneIcon} id="phone-icon"/>
 					</div>
